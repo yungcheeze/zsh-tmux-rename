b9cb837d5d91caab73e79ccdab9f4d2551d7d818
Ucizi Mafeni <ucizim@gmail.com>
2020-09-16 02:20:15 +0100

    make renames overridable by setting automatic-rename-format option instead of renaming windows

--- zsh-tmux-rename.plugin.zsh
+++ zsh-tmux-rename.plugin.zsh
@@ -8,9 +8,9 @@ rename-tmux-window() {
 
   LANG=en_US.UTF-8 vcs_info
   if [[ -n "$vcs_info_msg_0_" ]]; then
-    tmux rename-window "${vcs_info_msg_0_}"
+    tmux set-option -w automatic-rename-format "${vcs_info_msg_0_}"
   else
-    tmux rename-window "${PWD##*/}"
+    tmux set-option -w automatic-rename-format "${PWD##*/}"
   fi
 }
 
